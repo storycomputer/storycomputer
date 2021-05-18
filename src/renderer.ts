7aca52b1439b29e2ffecff18a630b70cf313d392
Stanislav Senotrusov <stan@senotrusov.com>
2021-05-18 04:24:53 +0200

    minor changes

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,9 +1,10 @@
+const sqlite = require('sqlite3');
 
 const aboutVersions = document.getElementById('about-versions');
 
 if (aboutVersions)
   aboutVersions.innerHTML = `
     Node.js ${process.versions.node},
-    SQLite ${require('sqlite3').VERSION},
+    SQLite ${sqlite.VERSION},
     Chrome ${process.versions.chrome},
     and Electron ${process.versions.electron}`;
